18578b737b028ddeac632650847b5b7c99ba572e
Felipe Lourenço <felipekauansilvalourenco35@gmail.com>
2025-03-31 14:50:03 -0300

    While05

---
+++ While/While05.py
@@ -0,0 +1,7 @@
+numero= int(input('Digite o número: '))
+
+while True:
+    numero = numero -1
+    print(numero)
+    if numero == 0:
+        break
---
+++ While/While06.py
@@ -0,0 +1,9 @@
+while True:
+    numero= int(input('Digite um  número positivo: '))
+
+    if numero < 0:
+        print(' O número deve ser positivo!')
+        continue
+    else:
+        print('Parabens o numero é positivo!!!!!')
+        break
